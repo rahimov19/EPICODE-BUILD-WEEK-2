a05801aaf92cba8867b35655f764e7392425edd2
Daniel <dan.earp@icloud.com>
2022-11-15 12:18:09 -0800

    Track list now updates

--- js/artists.js
+++ js/artists.js
@@ -14,8 +14,8 @@ const getArtist = async (artistID) => {
 
   const artist = await response.json()
 
-  console.log(artist.picture)
-  console.log(artist.picture_xl)
+  const getTracks = await fetch(artist.tracklist)
+  const allTracks = await getTracks.json()
 
   const artistName = document.querySelector('.artist')
   artistName.innerHTML = artist.name
@@ -29,4 +29,32 @@ const getArtist = async (artistID) => {
 
   const artistPicture = document.querySelector('.artist-pick-album-cover')
   artistPicture.setAttribute('src', artist.picture_xl)
+
+  const songContainer = document.querySelector('.songs-container')
+
+  allTracks.data.forEach((element) => {
+    songContainer.innerHTML += `<div class="song d-flex align-items-center mb-3">
+    <div class="track-number">
+      <i class="fa-solid fa-chart-simple mr-3"></i>
+    </div>
+    <div class="album-thumbnail-container">
+      <img
+        class="album-thumbnail mr-3"
+        src="${element.album.cover_small}"
+      />
+    </div>
+    <div class="song-title">
+      <span>
+        ${element.title}
+      </span>
+    </div>
+    <div class="play-counter">
+      <span>${element.rank.toLocaleString('en-US')}</span>
+    </div>
+    <span class="song-length mr-3">${element.duration}</span>
+  </div>`
+  })
+
+  const artistPickArtist = document.querySelector('.artist-pick-artist')
+  artistPickArtist.innerHTML = artist.name
 }
