5877571d17857679271bb11f780b7c833ff4785e
rahimov19 <rahimov19.ar@gmail.com>
2022-11-16 10:36:59 +0100

    songs part is impplemented

--- search.js
+++ search.js
@@ -17,15 +17,29 @@ async function getSongs() {
 }
 
 window.onload = async () => {
+  //   const listOfSongsSearch = await fillSearchSongs();
   searchBar.value = "hello";
   const listOfSearch = await getSongs();
   console.log(listOfSearch);
   fillLeft(listOfSearch);
   document.querySelector("#searchbar").addEventListener("keyup", getSongs2);
   document
+    .querySelector("#searchAllButton")
+    .addEventListener("click", function () {
+      returnToAll();
+    });
+  document
+    .querySelector("#searchAllButton")
+    .addEventListener("click", getSongs2);
+  document
     .querySelector("#searchbar")
     .addEventListener("click", function (event) {
       event.target.value = "";
+    });
+  document
+    .querySelector("#searchSongsButton")
+    .addEventListener("click", function () {
+      fillSearchSongs();
     });
 };
 
@@ -33,12 +47,44 @@ let leftSideSearch = document.querySelector("#leftSearch");
 let rightSideSeacrh = document.querySelector("#rightSearch");
 let albumsSearch = document.querySelector("#searchAlbums");
 let artistsSearch = document.querySelector("#searchArtists");
+let mainRow = document.querySelector("#mainrow");
 
 let clearAll = function () {
   leftSideSearch.innerHTML = "";
   rightSideSeacrh.innerHTML = "";
   albumsSearch.innerHTML = "";
   artistsSearch.innerHTML = "";
+};
+
+let returnToAll = function () {
+  document.querySelector(".main-content").innerHTML = "";
+  document.querySelector(
+    ".main-content"
+  ).innerHTML = `<div class="row" id="undersearch">
+    <div class="col-3" id="h3BR"><h3>Best Result:</h3></div>
+    <div class="col-9 pl-5" id="h3S"><h3>Songs:</h3></div>
+   
+</div>
+
+<div class="row" id="mainrow"> 
+    <div class="col-3" id="leftSearch">
+    <img src="" alt="">
+    <h3>Name of Album</h3>
+    <p>Name of Artist</p>
+</div>
+<div class="col-9" >
+    <ul id="rightSearch">
+        <li><img src="" alt=""><span>Name of Artist</span><span>Name of track</span>
+        <span>duration</span></li>
+    </ul>
+</div>
+</div>
+<h2 id="h2Albums">Albums</h2>
+<div class="row" id="searchAlbums">
+</div>
+<h2 id="h2Artists">Artists</h2>
+<div class="row" id="searchArtists">
+</div>`;
 };
 
 let fillLeft = function (listOfSearch) {
@@ -82,7 +128,7 @@ let fillLeft = function (listOfSearch) {
 };
 
 const getSongs2 = async (event) => {
-  if (event.keyCode === 13) {
+  if (event.keyCode === 13 || event.target.id === "searchAllButton") {
     try {
       const options = {
         method: "GET",
@@ -93,11 +139,13 @@ const getSongs2 = async (event) => {
         },
       };
       const response = await fetch(
-        `https://striveschool-api.herokuapp.com/api/deezer/search?q=${event.target.value}`,
+        `https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchBar.value}`,
         options
       );
       if (response.ok) {
         const listOfSearch = await response.json();
+        console.log(listOfSearch);
+        // CHANGE CLASSES OF BUTTONS TO IF STATEMENT
         fillLeft(listOfSearch);
       } else {
         console.log("Error while fetching");
@@ -112,7 +160,6 @@ let allButton = document.querySelector("#searchAllButton");
 let songsButton = document.querySelector("#searchSongsButton");
 let albumsButton = document.querySelector("#searchAlbumsButton");
 let artistsButton = document.querySelector("#searchArtistsButton");
-let mainRow = document.querySelector("#mainRow");
 
 const fillSearchSongs = async () => {
   try {
@@ -128,20 +175,60 @@ const fillSearchSongs = async () => {
       options
     );
     if (response.ok) {
-      const listOfSearch = await response.json();
-      clearAll();
-      mainRow.innerHTML = "";
-      mainRow.innerHTML = `
-<div class="col-12"> 
-<ul class="songsUl">
-<li>
-</ul>
-</div>
-`;
+      const listOfSongsSearch = await response.json();
+      console.log(listOfSongsSearch);
+      fillSongs(listOfSongsSearch);
     } else {
       console.log("Error while fetching");
     }
   } catch (error) {
     console.error(error);
   }
+};
+let fillSongs = function (listOfSearch) {
+  clearAll();
+  document.querySelector("#h2Albums").innerHTML = "";
+  document.querySelector("#h2Artists").innerHTML = "";
+  document.querySelector(
+    "#h3BR"
+  ).innerHTML = `<h3 class="mb-3">Search Results:</h3>`;
+  document.querySelector("#h3S").innerHTML = "";
+
+  mainRow.innerHTML = `<table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Title</th>
+          <th scope="col">Album</th>
+          <th scope="col">Duration</th>
+        </tr>
+      </thead>
+      
+    <tbody id="tablebody"></tbody>
+    `;
+  for (i = 0; i < 20; i++) {
+    document.querySelector("#tablebody").innerHTML += `
+        <tr>
+        <th scope="row">${[i + 1]}</th>
+        <td><div class="songtitle"><img src=${
+          listOfSearch.data[i].album.cover_small
+        } alt="">
+        <div class="spanText col-10"><span>${
+          listOfSearch.data[i].artist.name
+        }</span>
+        <span>${listOfSearch.data[i].title}</span></div></td>
+        <td>${listOfSearch.data[i].album.title}</td>
+        <td>${
+          (listOfSearch.data[i].duration -
+            (listOfSearch.data[i].duration %= 60)) /
+            60 +
+          (9 < listOfSearch.data[i].duration ? ":" : ":0") +
+          listOfSearch.data[i].duration
+        }</td>
+      </tr>
+
+  `;
+  }
+  mainRow.innerHTML += `
+</table>`;
 };
