6c90c3574c20399b568accc52487fc5ed2978eb5
Daniel <dan.earp@icloud.com>
2022-11-17 08:16:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,88 @@
 async function getAlbums() {
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "0804dffc02mshffe59d44538faefp143e0bjsne323b0c03419",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      'X-RapidAPI-Key': '0804dffc02mshffe59d44538faefp143e0bjsne323b0c03419',
+      'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
     },
-  };
+  }
   const response = await fetch(
     `https://striveschool-api.herokuapp.com/api/deezer/search?q=hello`,
-    options
-  );
+    options,
+  )
 
-  const listOfSearch = await response.json();
-  return listOfSearch;
+  const listOfSearch = await response.json()
+  return listOfSearch
 }
 
 async function getSongs() {
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "0804dffc02mshffe59d44538faefp143e0bjsne323b0c03419",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      'X-RapidAPI-Key': '0804dffc02mshffe59d44538faefp143e0bjsne323b0c03419',
+      'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
     },
-  };
+  }
   const response = await fetch(
     `https://striveschool-api.herokuapp.com/api/deezer/search?q=skillet`,
-    options
-  );
+    options,
+  )
 
-  const listOfSongs = await response.json();
-  return listOfSongs;
+  const listOfSongs = await response.json()
+  return listOfSongs
 }
 
 async function getArtist() {
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "0804dffc02mshffe59d44538faefp143e0bjsne323b0c03419",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      'X-RapidAPI-Key': '0804dffc02mshffe59d44538faefp143e0bjsne323b0c03419',
+      'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
     },
-  };
+  }
   const response = await fetch(
     `https://striveschool-api.herokuapp.com/api/deezer/search?q=song`,
-    options
-  );
+    options,
+  )
 
-  const listOfArtists = await response.json();
-  return listOfArtists;
+  const listOfArtists = await response.json()
+  return listOfArtists
 }
 
 window.onload = async () => {
-  const listOfSearch = await getAlbums();
-  const listOfSongs = await getSongs();
-  const listOfArtists = await getArtist();
-  console.log(listOfArtists);
-  console.log(listOfSearch);
-  console.log(listOfSongs);
-  fillPageAlbums(listOfSearch);
-  fillPageSongs(listOfSongs);
-  fillPageArtists(listOfArtists);
-};
+  const listOfSearch = await getAlbums()
+  const listOfSongs = await getSongs()
+  const listOfArtists = await getArtist()
+  console.log(listOfArtists)
+  console.log(listOfSearch)
+  console.log(listOfSongs)
+  fillPageAlbums(listOfSearch)
+  fillPageSongs(listOfSongs)
+  fillPageArtists(listOfArtists)
+}
 
-let topsidecards = document.querySelector("#topsidecards");
-let firstRow = document.querySelector("#firstAlbumRow");
-let secondRow = document.querySelector("#secondAlbumRow");
+let topsidecards = document.querySelector('#topsidecards')
+let firstRow = document.querySelector('#firstAlbumRow')
+let secondRow = document.querySelector('#secondAlbumRow')
 
 const fillPageAlbums = function (listOfSearch) {
-  topsidecards.innerHTML = "";
+  topsidecards.innerHTML = ''
   for (i = 0; i < 10; i++) {
-    topsidecards.innerHTML += `<a href="albums.html?albumID=${listOfSearch.data[i].album.id}>"<div class="sidecards col-2">
+    topsidecards.innerHTML += `<a href="albums.html?albumID=${listOfSearch.data[i].album.id}"<div class="sidecards col-2">
 <img
   class="col-4"
   src="${listOfSearch.data[i].album.cover_medium}"
   alt=""
 />
 <p class="col-8 sidetext">${listOfSearch.data[i].album.title}</p>
-</div></a>`;
+</div></a>`
   }
-};
+}
 
 const fillPageSongs = function (listOfSongs) {
-  firstRow.innerHTML = "";
+  firstRow.innerHTML = ''
   for (i = 0; i < 8; i++) {
-    firstRow.innerHTML += `<a href="artists.html?artistID=${listOfSongs.data[i].artist.id}>"<div class="albumCard">
+    firstRow.innerHTML += `<a href="artists.html?artistID=${listOfSongs.data[i].artist.id}"<div class="albumCard">
         <img
           class="col-11"
           src="${listOfSongs.data[i].album.cover_medium}"
@@ -92,14 +92,14 @@ const fillPageSongs = function (listOfSongs) {
           <p>${listOfSongs.data[i].artist.name}</p>
           <p>${listOfSongs.data[i].title}</p>
         </div>
-      </div></a>`;
+      </div></a>`
   }
-};
+}
 
 const fillPageArtists = function (listOfArtists) {
-  secondRow.innerHTML = "";
+  secondRow.innerHTML = ''
   for (i = 0; i < 8; i++) {
-    secondRow.innerHTML += `<a href="albums.html=albumID?${listOfArtists.data[i].album.id}>"<div class="albumCard">
+    secondRow.innerHTML += `<a href="albums.html?albumID=${listOfArtists.data[i].album.id}"<div class="albumCard">
           <img
             class="col-11"
             src="${listOfArtists.data[i].artist.picture}"
@@ -108,6 +108,6 @@ const fillPageArtists = function (listOfArtists) {
           <div class="albumText">
             <p>${listOfArtists.data[i].artist.name}</p>
             </div>
-        </div></a>`;
+        </div></a>`
   }
-};
+}
